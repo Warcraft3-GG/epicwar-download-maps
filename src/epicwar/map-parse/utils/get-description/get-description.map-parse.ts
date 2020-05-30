46b9021b85455546e5eff831116e4ddd8526db18
Sansossio <juliosansossio@gmail.com>
2020-05-30 10:54:18 +0200

    Clear code

--- src/epicwar/map-parse/utils/get-description/get-description.map-parse.ts
+++ src/epicwar/map-parse/utils/get-description/get-description.map-parse.ts
@@ -6,13 +6,14 @@ export function getMapDescription (content: string): string | null {
 
   for (const cell of cells) {
     const content = cell.textContent
-    if (content.indexOf('Share this map') > -1) {
-      const innerHTML = cell.innerHTML
-      const html = innerHTML
-        .split(/by <b>(.*?)<\/b><br><br>/gm)[2].trim()
-        .split(/<br><br>\n\s+<b>Share this map: <\/b>/gm)[0].trim()
-      return html
+    if (content.indexOf('Share this map') === -1) {
+      continue
     }
+    const innerHTML = cell.innerHTML
+    const html = innerHTML
+      .split(/by <b>(.*?)<\/b><br><br>/gm)[2].trim()
+      .split(/<br><br>\n\s+<b>Share this map: <\/b>/gm)[0].trim()
+    return html
   }
 
   return null
