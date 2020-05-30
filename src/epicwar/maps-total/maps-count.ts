45b2c9cd9f824fbbb7543e1b0db2b22663fb4bf4
Sansossio <juliosansossio@gmail.com>
2020-05-30 13:20:50 +0200

    Update function name

--- src/epicwar/maps-total/maps-count.ts
+++ src/epicwar/maps-total/maps-count.ts
@@ -2,7 +2,10 @@ import axios from 'axios'
 import { JSDOM } from 'jsdom'
 import { EpicwarEnum } from '../epicwar.enum'
 
-export async function mapsTotal () {
+/**
+ * Return total maps in epicwards.com
+ */
+export async function totalMaps () {
   const homepage = await axios(EpicwarEnum.BASE_URL)
   const html = new JSDOM(homepage.data)
   const urls = html.window.document.getElementsByTagName('a')
