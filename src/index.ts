5355864b5d453fe0db7f0dc9304359d80dcd3284
Sansossio <juliosansossio@gmail.com>
2020-05-30 13:20:36 +0200

    Export functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-import { epicwarGetMap } from './epicwar/get-map/epicwar.get-map'
-import { mapsTotal } from './epicwar/maps-total/maps-count'
-
-(async () => {
-  const mapParse = await epicwarGetMap(1)
-  console.log(mapParse)
-  // const totalMaps = await mapsTotal()
-  // console.log(totalMaps)
-})()
+export { epicwarGetMap } from './epicwar/get-map/epicwar.get-map'
+export { totalMaps } from './epicwar/maps-total/maps-count'
